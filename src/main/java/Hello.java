7798fe390f907d57f36ca9105471c6fd15333666
olena333 <akp2315@gmail.com>
2021-01-26 15:22:50 -0500

    movie1 updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, B-21");
         System.out.println("B-21");
+        //watching more movie in a living room
     }
 }
